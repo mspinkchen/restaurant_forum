3b26e79b40cd8ea35afd3278452d716d5220d804
mspink <arpinkchen@gmail.com>
2017-11-30 21:52:51 +0800

    add login process and update view with login message

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :restaurants, only: [:index, :show]
-  root "restaurant#index"
+  root "restaurants#index"
 
 
   namespace :admin do
-    root "restaurant#index"
+    root "restaurants#index"
     resources :restaurants
   end
 end
