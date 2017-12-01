f84a48010ea5bbe387c3c505a99a95b60177caaa
mspink <arpinkchen@gmail.com>
2017-12-01 19:01:17 +0800

    add restaurant new and create actions and vies for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,34 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
 
   def index
+    @restaurants = Restaurant.all
   end
+
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash[:alert] = "Restaurant was faild to create"
+      render :new
+    end
+  end
+
+
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hour, :description)
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  private
+  def authenticate_admin
+    if !current_user.admin?
+      flash[:alert]="not allow"
+      redirect_to root_path
+  end
+  end
+
 end
