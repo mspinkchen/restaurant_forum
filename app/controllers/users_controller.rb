4187723dc27d55e727ddc9ccc941d64cdbb4e59e
mspink <arpinkchen@gmail.com>
2017-12-26 21:35:07 +0800

    user can update profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show , :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     end
+
+  def edit
+    if !@user == current_user
+      redirect_to user_path(@user)
+    end
+
+
+  end
+
+  def update
+    @user = User.update(user_params)
+      redirect_to user_path(@user)
+
+    end
+    end
+
+    private
+
+      def set_user
+        @user = User.find(params[:id])
+
+      def user_params
+        params.require(:user).permit(:name, :intro,)
+
+      end
 
 
 end
