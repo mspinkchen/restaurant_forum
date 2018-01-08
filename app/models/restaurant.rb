d679e1f6d38b5c57c26aea171fef4ec48d12d332
mspink <arpinkchen@gmail.com>
2018-01-08 22:18:38 +0800

    add is_liked method to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,6 +15,10 @@ class Restaurant < ApplicationRecord
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
   
 
 end
