5707b07beda9f86b75968b703e6c703853e7986e
mspink <arpinkchen@gmail.com>
2018-01-11 21:19:06 +0800

    create friendship model and setup self-referential relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,9 @@ class User < ApplicationRecord
   has_many :inverse_followship, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followship, source: :user
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
   def admin?
     self.role == "admin"
   end
