0424e392b04f480cca56708be2227805b369a71c
mspink <arpinkchen@gmail.com>
2018-01-17 20:05:53 +0800

    setup route for ranking page

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,13 @@ Rails.application.routes.draw do
 
     collection do 
       get :feeds
+      get :ranking
     end
 
     member do
       get :dashboard
-    end
-
-    member do
       post :favorite
       post :unfavorite
-    end
-
-    member do
       post :like
       post :unlike
     end
