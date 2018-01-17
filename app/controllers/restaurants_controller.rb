a38e94d379bf48aa02027742b9a8dbc466295014
mspink <arpinkchen@gmail.com>
2018-01-17 22:01:20 +0800

    user can view top10 favorited restaurant on ranking page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,6 @@
 class RestaurantsController < ApplicationController
 
   before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
-  
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -24,27 +23,32 @@ class RestaurantsController < ApplicationController
 
   def favorite
      @restaurant.favorites.create(user: current_user)
+     @restaurant.count_favorites
      redirect_back(fallback_location: root_path)
    end
  
 
-   def unfavorite 
+  def unfavorite 
      favorites = Favorite.where(restaurant: @restaurant, user: current_user)
      favorites.destroy_all
+     @restaurant.count_favorites
      redirect_back(fallback_location: root_path)
-   end
+  end
 
-   def like
+  def like
     @restaurant.likes.create!(user: current_user)
     redirect_back(fallback_location: root_path)
-   end
+  end
 
-   def unlike
+  def unlike
     likes = Like.where(restaurant: @restaurant, user: current_user)
     likes.destroy_all
     redirect_back(fallback_location: root_path)
-   end
+  end
      
+  def ranking
+    @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
 
    private 
 
